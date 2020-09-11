ff92624f313fe7e7d0bc9109f5a28fc5da72bf00
Santeri <santeri.j.kaariainen@gmail.com>
2020-09-11 07:38:26 +0000

    expose postgres

--- tileserver-raster/run.sh
+++ tileserver-raster/run.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 docker run \
     -p 8090:80 \
+    -p 5424:5432 \
     -e UPDATES=enabled \
     -v openstreetmap-data:/var/lib/postgresql/12/main \
     -v openstreetmap-rendered-tiles:/var/lib/mod_tile \
